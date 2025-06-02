cb92b4e87e168e9beab1ee216efb0fbddc7f4f09
Nikolai Stanes <nikolaistanes@gmail.com>
2025-06-02 11:31:19 +0200

    Tredje commit
    Jeg valgte å begynne på nytt

--- Nettside/src/App.tsx
+++ Nettside/src/App.tsx
@@ -1,83 +1,34 @@
-import { useState, useEffect } from 'react'
-import Foresporsler from './komponenter/foresporsler';
-import './style.css'
-
-interface Hund
-{
-  id: number;
-  navn: string;
-  rase: string;
-  alder: number;
-  spesielleBehov: string;
-}
-
-interface HundeEier
-{
-  id: number;
-  navn: string;
-  telefon: string;
-  email: string;
-  adresse: string;
-  hund: Hund;
-}
-
-interface HundePasser
-{
-  id: number;
-  navn: string;
-  telefon: string;
-  email: string;
-  pris: number;
-  omraade: string;
-}
-
-interface Foresporsel
-{
-  id: number;
-  eier: HundeEier;
-  passer: HundePasser;
-  dato: Date;
-  instruksjoner: string;
-  akseptert: boolean;
-}
+import {useState} from 'react';
+import LoggPaaSkjema from './komponenter/loggpaa';
 
 interface Bruker
 {
-  id: number;
-  brukernavn: string;
-  passord: string;
-  rolle: string;
+  id:number;
+  brukernavn:string;
+  passord:string;
+
 }
 
-const App = () => {
+const App = () =>{
 
-  const [hundeEiere, setHundeEiere] = useState<HundeEier[]>([]);
-  const [hundePassere, setHundePassere] = useState<HundePasser[]>([]);
-  const [foresporsler, setForesporsler] = useState<Foresporsel[]>([]);
-  
   const [loggetPaa, setLoggetPaa] = useState(false);
-  const [burkernavn, setBrukernavn] = useState("");
+  const [brukernavn, setBrukernavn] = useState("");
   const [passord, setPassord] = useState("");
-  const [aktivBruker, setAktivBruker] = useState<Bruker>();
+  const [lagerBruker, setLagerBruker] = useState(false);
 
-  const login = () => {
-    console.log("Login");
+  const Login = () =>{
     setLoggetPaa(true);
   }
 
+  const LagBruker = () =>{
+
+  }
+
   return (
-    <>
-      {!loggetPaa ?   
-      <form className="login" onSubmit={e=>{e.preventDefault(); login()}}>
-        <input required placeholder="Brukernavn" value={burkernavn} onChange={e=>setBrukernavn(e.target.value)}/>
-        <input required placeholder="Passord" value={passord} onChange={e=>setPassord(e.target.value)}/>
-        <button type="submit">Logg inn</button>
-      </form>
-    : <div className="hovedSide">
-        {aktivBruker && <Foresporsler aktivBruker={aktivBruker} foresporsler={foresporsler}/>}
-      </div>}
-    </>
-  )
+  <>
+  {loggetPaa ? <h1>Hi</h1>:<LoggPaaSkjema brukernavn={brukernavn} passord={passord} setBrukernavn={setBrukernavn} setPassord={setPassord} Login={Login} lagerBruker={lagerBruker} setLagerBruker={setLagerBruker} LagBruker={LagBruker}/>}
+  </>
+  );
 }
 
-export default App
+export default App;
---
+++ Nettside/src/komponenter/loggpaa.tsx
@@ -0,0 +1,24 @@
+interface props
+{
+    brukernavn: string;
+    passord: string;
+    setBrukernavn: (verdi:string)=>void;
+    setPassord: (verdi:string)=>void;
+    Login:()=>void;
+    lagerBruker:boolean;
+    setLagerBruker:(verdi:boolean)=>void;
+    LagBruker:()=>void;
+}
+
+const LoggPaaSkjema = ({brukernavn, passord, setBrukernavn, setPassord, Login, lagerBruker, setLagerBruker, LagBruker}:props) =>{
+    return (
+    <form onSubmit={e=>{e.preventDefault(); (lagerBruker ? LagBruker : Login())}}>
+    <input required value={brukernavn} placeholder="Brukernavn" onChange={e=>setBrukernavn(e.target.value)}/>
+    <input required value={passord} placeholder="Passord" onChange={e=>setPassord(e.target.value)}/>
+    <button type="submit">{lagerBruker ? "Lag bruker" : "Logg inn"}</button>
+    <button onClick={()=>setLagerBruker((lagerBruker ? false : true))}>{lagerBruker ? "Har bruker?" : "Har ikke bruker?"}</button>
+    </form>
+    )
+}
+
+export default LoggPaaSkjema;
